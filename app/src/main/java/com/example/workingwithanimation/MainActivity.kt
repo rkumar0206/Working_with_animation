070b5f6659715d5da3301619be646c6d6ba040d6
Mohit Kumar <mohitk02082000@gmail.com>
2020-08-25 21:17:02 +0530

    minor change

--- app/src/main/java/com/example/workingwithanimation/MainActivity.kt
+++ app/src/main/java/com/example/workingwithanimation/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.workingwithanimation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initListeners()
+    }
+
+    private fun initListeners() {
+
+        rotateBtn.setOnClickListener(this)
+        translateBtn.setOnClickListener(this)
+        scaleBtn.setOnClickListener(this)
+        fadeBtn.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+
+        when (v?.id) {
+
+
+        }
     }
 }
